d2f25c9e3d6cc6bc292eca2c4d9ca7fe6df23258
hijira <hijira.seid@gmail.com>
2022-06-08 01:15:52 -0700

    sixth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include < stdio.h >                                                                                                            
 
+/*                                                                                                                              
 
+ * main - prints a string                                                                                                       
 
-/* main - prints a string
+ * Description : prints "programming is like building a multilingual puzzel"                                                    
 
- * Description: prints “Programming is like building a multilingual puzzle
+ * Return : 0                                                                                                                   
 
- *Return: 0
+ */                                                                                                                             
 
- */
+int main(void) // main function
+{                                                                                                                               
 
-int main(void)
+puts("\"programming is like building a mutiligual puzzle");                                                                     
 
-{
+return (0);                                                                                                                     
 
-puts(“\”Programming is like building a multilingual puzzle”);
-
-return (0);}
-
+}     
