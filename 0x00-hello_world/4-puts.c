83b31cabdc35a70acdfcf8aedf929dff852a95c6
hijira <hijira.seid@gmail.com>
2022-06-10 06:23:05 -0700

    no4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
-#include < stdio.h >                                                                                                            
+#include <stdio.h>
 
-/*                                                                                                                              
-
- * main - prints a string                                                                                                       
-
- * Description : prints "programming is like building a multilingual puzzel"                                                    
-
- * Return : 0                                                                                                                   
-
- */                                                                                                                             
-
-int main(void) // main function
-{                                                                                                                               
-
-puts("\"programming is like building a mutiligual puzzle");                                                                     
-
-return (0);                                                                                                                     
-
-}     
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main void
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
