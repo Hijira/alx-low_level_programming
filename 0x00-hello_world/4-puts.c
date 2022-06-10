d2a8a02ece264bd219c3ddf3cd0a505203d4110f
hijira <hijira.seid@gmail.com>
2022-06-10 06:46:22 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+
+ * main - Entry Point
+
+ *
+
+ * Return: Always 0 (success)
+
+ */
+
 int main(void)
+
 {
-	    return (0);
-} 
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
