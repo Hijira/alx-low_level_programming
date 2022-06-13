9863edba50fa265dfc748a6a97daf92e0574a76d
hijira <hijira.seid@gmail.com>
2022-06-13 05:27:45 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
+	
 
+	/**
 
-/**
+	 * main - Entry Point
 
- * main - Entry Point
+	 *
 
- *
+	 * Return: Always 0 (success)
 
- * Return: Always 0 (success)
+ * betty style doc for function main goes there
 
- */
+	 */
 
-int main(void)
+	int main(void)
 
-{
+	{
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+		return (0);
 
-}
+	}
+
