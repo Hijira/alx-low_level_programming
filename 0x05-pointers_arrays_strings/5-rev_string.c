c8ce41fb506315c2ff6a341a6fdee0a2829c9ddb
hijira <hijira.seid@gmail.com>
2022-06-14 01:16:10 -0700

    noseven

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
- * print_rev - prints a reverse string.
- * @s: input string.
- * Return: no return.
- * betty style doc for function main goes there
- */
+* print_rev - prints a reverse string.
+* @s: input string.
+* Return: no return.
+* betty style doc for function main goes there
+*/
 void print_rev(char *s)
 {
-	int str_rev = 0, a, b;
-	char *str, ver;
-	while (str_rev >= 0)
-	{
-	if (s[str_rev] == '\0')
-		break;
-	str_rev++;
-	}
-	str = s;
-	for (a = 0; a < (str_rev - 1); a++)
-	{
-		for (b = a + 1; b > 0; b--)
-		{
-			ver = *(str + b);
-			*(str + b) = *(str + (b - 1));
-			*(str + (b - 1)) = ver;
-		}
-	}
+int str_rev = 0, a, b;
+char *str, ver;
+while (str_rev >= 0)
+{
+if (s[str_rev] == '\0')
+break;
+str_rev++;
+}
+str = s;
+for (a = 0; a < (str_rev - 1); a++)
+{
+for (b = a + 1; b > 0; b--)
+{
+ver = *(str + b);
+*(str + b) = *(str + (b - 1));
+*(str + (b - 1)) = ver;
+}
+}
 }
