64e2d8093989af8397541dc61e0341488b280c48
hijira <hijira.seid@gmail.com>
2022-06-16 01:43:07 -0700

    second

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strncpy - string copycat
+ * @dest: destination.
+ * @src: source.
+ * @n: bytes amount from src.
+ * Return: dest pointer
+ * betty style doc for function main goes there
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int str;
+for (str = 0; str < n && src[i] != '\0'; str++)
+dest[i] = src[i];
+for ( ; str < n; str++)
+dest[i] = '\0';
+return (dest);
+}
