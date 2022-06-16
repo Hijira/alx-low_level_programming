4857fede8a99788896728b489e68270a41a938fd
hijira <hijira.seid@gmail.com>
2022-06-16 02:48:13 -0700

    advcor1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 /**
- * main - print sum of even Fionacci sequence up to 4,000,000
- * Return: 0
+ * main - Prints the add of the Fibonacci numbers
+ * Return: Always 0.
  * betty style doc for function main goes there
  */
 int main(void)
 {
-int even_sum = 0;
-int a;
-int b;
-int sum = 1;
-a = 1;
-b = 1;
-while (b < 4000000)
+int c;
+unsigned long a =0, b = 1, d;
+for (d = 0; d < 50; d++)
 {
-sum = a + b;
+d = a + b;
+printf("%lu", d);
 a = b;
-b = sum;
-if ((sum <= 4000000) && (sum % 2 == 0))
-even_sum += sum;
+b = d;
+if(c == 49)
+printf("\n");
+else
+printf(", ");
 }
-printf("%d\n", even_sum);
 return (0);
 }
