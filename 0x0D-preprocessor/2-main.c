fbafc096f0ade419b4c2bd010c5bcf22d1334f0a
Hijira <hijira.seid@gmail.com>
2022-10-10 11:00:34 -0700

    All together

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file.
+ * Return: Always 0.
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
